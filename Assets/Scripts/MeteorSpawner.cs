3a0707ef32131a670814d715a060e42c72ef9a94
victorcostap <83207265+victorcostap@users.noreply.github.com>
2023-11-04 09:32:11 +0100

    Resolve warning in MeteorSpawner

--- Assets/Scripts/MeteorSpawner.cs
+++ Assets/Scripts/MeteorSpawner.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MeteorSpawner : MonoBehaviour
@@ -11,20 +9,20 @@ public class MeteorSpawner : MonoBehaviour
     private float _spawnNext = 0;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if(Time.time > _spawnNext){
-            _spawnNext = Time.time + (60/spawnPerMinute);
-            spawnPerMinute += spawnRateIncrement;
+        if (Time.time < _spawnNext) return;
+        
+        _spawnNext = Time.time + (60/spawnPerMinute);
+        spawnPerMinute += spawnRateIncrement;
 
-            float randX = Random.Range(-10,10);
-            var spawnPosition = new Vector2(randX, 
-                                            transform.position.y);
-            Instantiate(meteorPrefab, spawnPosition, Quaternion.identity);
-        }
+        float randX = Random.Range(-10,10);
+        var spawnPosition = new Vector2(randX, 
+                                        transform.position.y);
+        Instantiate(meteorPrefab, spawnPosition, Quaternion.identity);
     }
 
-    void OnBecameInvisible()
+    private void OnBecameInvisible()
     {
         Destroy(meteorPrefab);
     }

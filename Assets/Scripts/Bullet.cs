ada8d5eb0b79d037ae47f3c4d7c56e8bd6462f85
victorcostap <83207265+victorcostap@users.noreply.github.com>
2023-11-04 19:04:20 +0100

    Create bullet and bullet spawner

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class Bullet : PooledObject
+{
+    private Rigidbody _rigidbody;
+    
+    private void Start()
+    {
+        _rigidbody = this.gameObject.GetComponent<Rigidbody>();
+    }
+    
+    private new void OnBecameInvisible()
+    {
+        _rigidbody.velocity = Vector3.zero;
+        _rigidbody.rotation = Quaternion.identity;
+        base.OnBecameInvisible();
+    }
+}
---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class BulletSpawner : ObjectSpawnerBase
+{
+    public float delayMsBullet = 250;
+    public float bulletSpeed = 10;
+    private float _spawnNext;
+    private Vector3 _spawnOffset = new Vector3(1,0,0);
+
+    private void Update()
+    {
+        if (!(Input.GetKey(KeyCode.Space) && Time.time > _spawnNext)) return;
+        
+        _spawnNext = Time.time + delayMsBullet / 1000;
+        var bullet = GetPooledObject();
+        if (!bullet) return;
+        bullet.transform.position = transform.position +
+                                    transform.TransformDirection(_spawnOffset);
+        var rb = bullet.GetComponent<Rigidbody>();
+        //rb.AddForce(transform.right * bulletSpeed);
+        rb.velocity = transform.right * bulletSpeed;
+        rb.rotation = Quaternion.identity;
+        bullet.SetActive(true);
+    }
+}

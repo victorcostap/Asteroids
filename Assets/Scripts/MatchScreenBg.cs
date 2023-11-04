67e36cbe3745df16b059e4e7e44526b4645d0abe
victorcostap <83207265+victorcostap@users.noreply.github.com>
2023-11-04 13:13:19 +0100

    WIP Match BG to screen

---
+++ Assets/Scripts/MatchScreenBg.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class MatchScreenBg : MonoBehaviour
+{
+    private Vector2 _screenResolution;
+    private Camera _camera;
+    private float _objectWidth;
+    private float _objectHeight;
+    private float _aspectRatio;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _camera = Camera.main;
+        if (_camera == null)
+        {
+            Debug.LogError("No main camera found.");
+            return;
+        }
+
+        _screenResolution = new Vector2(Screen.width, Screen.height);
+        MatchPlaneToScreenSize();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (_screenResolution.Equals(new Vector2(Screen.width, Screen.height))) return;
+        
+        MatchPlaneToScreenSize();
+        _screenResolution.x = Screen.width;
+        _screenResolution.y = Screen.height;
+    }
+
+    private void MatchPlaneToScreenSize()
+    {
+        var planeHeightScale = 2.0f * _camera.orthographicSize / 10.0f;
+        var planeWidthScale = planeHeightScale * _camera.aspect;
+        gameObject.transform.localScale = new Vector3(planeWidthScale, planeHeightScale, 1);
+    }
+}

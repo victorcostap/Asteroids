4888f9edc4f785f186325be071709c4868527362
Victor Costa <victor.costa.perez@upm.es>
2023-11-16 13:58:29 +0100

    Avoid getting rigidBody each time bullet is spawned

--- Assets/Scripts/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner.cs
@@ -1,25 +1,32 @@
 ﻿using System;
 using UnityEngine;
 
-public class BulletSpawner : ObjectSpawnerBase
+public class BulletSpawner : ObjectSpawnerRigidBodyBase
 {
     public float delayMsBullet = 250;
     public float bulletSpeed = 10;
     private float _spawnNext;
-    private Vector3 _spawnOffset = new Vector3(1,0,0);
+    private readonly Vector3 _spawnOffset = new Vector3(1,0,0);
+    private Transform _tr;
 
+    private new void Start()
+    {
+        base.Start();
+        _tr = transform;
+    }
+    
     private void Update()
     {
         if (!(Input.GetKey(KeyCode.Space) && Time.time > _spawnNext)) return;
         
         _spawnNext = Time.time + delayMsBullet / 1000;
-        var bullet = GetPooledObject();
-        if (!bullet) return;
-        bullet.transform.position = transform.position +
-                                    transform.TransformDirection(_spawnOffset);
-        var rb = bullet.GetComponent<Rigidbody>();
-        //rb.AddForce(transform.right * bulletSpeed);
-        rb.velocity = transform.right * bulletSpeed;
+        var bulletRb = GetPooledObject();
+        if (bulletRb==null) return;
+        var bullet = bulletRb.Item1;
+        bullet.transform.position = _tr.position +
+                                    _tr.TransformDirection(_spawnOffset);
+        var rb = bulletRb.Item2;
+        rb.velocity = _tr.right * bulletSpeed;
         rb.rotation = Quaternion.identity;
         bullet.SetActive(true);
     }
---
+++ Assets/Scripts/ObjectSpawnerRigidBodyBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawnerRigidBodyBase : ObjectSpawnerBase
+{
+    
+    private Dictionary<int, Rigidbody> _object2Rb = new Dictionary<int, Rigidbody>();
+    
+    protected new void Start()
+    {
+        base.Start();
+        for (uint i = 0; i < base.maxObjects; ++i)
+        {
+            var b = base.GetPooledObject();
+            var rb = b.GetComponent<Rigidbody>();
+            _object2Rb.Add(b.GetInstanceID(), rb);
+            ReturnToPool(b);
+        }
+    }
+
+    public new Tuple<GameObject, Rigidbody> GetPooledObject()
+    {
+        var b = base.GetPooledObject();
+        return !b ? 
+            null : 
+            new Tuple<GameObject, Rigidbody>(b, _object2Rb[b.GetInstanceID()]);
+    }
+}

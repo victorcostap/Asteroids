0be1a8ed5085df3c340b108973b4307b879cdc29
Victor Costa <victor.costa.perez@upm.es>
2023-11-17 22:12:43 +0100

    Add buttons to title screen

---
+++ Assets/Scripts/ButtonUISceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonUISceneLoader : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
--- Assets/Scripts/TextBobbing.cs
+++ Assets/Scripts/TextBobbing.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class TextBobbing : MonoBehaviour
 {
-    public float bobbingAmount = 0.1f;
+    public float verticalBobbingAmount = 0.1f;
+    public float horizontalBobbingAmount = 0.1f;
     public float bobbingSpeed = 1f;
+    public float updateInterval = 0.5f; // Update every 0.5 seconds
 
     private Vector3 _originalPosition;
+    private float _timer = 0f;
 
     private void Start()
     {
@@ -14,8 +17,20 @@ public class TextBobbing : MonoBehaviour
 
     private void Update()
     {
+        // Increment the timer
+        _timer += Time.deltaTime;
+
+        if (_timer < updateInterval) return;
+
         // Apply bobbing animation to the Y position of the text
-        var newY = _originalPosition.y + Mathf.Sin(Time.time * bobbingSpeed) * bobbingAmount;
-        transform.position = new Vector3(_originalPosition.x, newY, _originalPosition.z);
+        var newY = _originalPosition.y + Mathf.Sin(Time.time * bobbingSpeed) * verticalBobbingAmount;
+
+        // Apply bobbing animation to the X position of the text
+        var newX = _originalPosition.x + Mathf.Cos(Time.time * bobbingSpeed) * horizontalBobbingAmount;
+
+        transform.position = new Vector3(newX, newY, _originalPosition.z);
+
+        // Reset the timer
+        _timer = 0f;
     }
 }

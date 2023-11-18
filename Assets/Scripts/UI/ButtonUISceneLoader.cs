1983cb2309d6fc76f05b552938c956a9e5dfcc5a
victorcostap <83207265+victorcostap@users.noreply.github.com>
2023-11-18 18:39:22 +0100

    Fix buttons not working and added option to quit

--- Assets/Scripts/UI/ButtonUISceneLoader.cs
+++ Assets/Scripts/UI/ButtonUISceneLoader.cs
@@ -3,10 +3,27 @@ using UnityEngine.SceneManagement;
 
 public class ButtonUISceneLoader : MonoBehaviour
 {
-    public string sceneToLoad;
-
-    public void LoadScene()
+    public string sceneNewGame;
+    public string sceneScores;
+    public string sceneCredits;
+    
+    public void NewGame()
     {
-        SceneManager.LoadScene(sceneToLoad);
+        SceneManager.LoadScene(sceneNewGame);
+    }
+
+    public void Scores()
+    {
+        
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(sceneCredits);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
